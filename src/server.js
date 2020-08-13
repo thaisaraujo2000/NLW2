f4031ed99a6b9517bda8415e469266d622824e2c
thaisaraujo2000 <56180384+thaisaraujo2000@users.noreply.github.com>
2020-08-13 03:25:41 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,30 @@
+//Servidor
+const express = require('express')
+const server = express()
+
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require("./pages")
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+// Servidor
+server
+// Receber os dados do req.body
+.use(express.urlencoded({ extended: true}))
+// Configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+// Rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+// Start do servidor
+.listen(5500)
